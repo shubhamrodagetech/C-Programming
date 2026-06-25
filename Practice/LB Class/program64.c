fe37646cedb87d80525cc11230eb83207422afbd
shubhamrodagetech <rodageshubham756@gmail.com>
2026-06-25 10:33:33 +0530

    Add files via upload

---
+++ Practice/LB Class/program64.c
@@ -0,0 +1,30 @@
+// input : 5
+// output : -5 -4 -3 -2 -1 0 1 2 3 4 5
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = -iNo; iCnt <= iNo; iCnt++)        // change line before code
+    {
+        printf("%d\t",iCnt);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
+
+// time complexicty = 2n
